f6ba5950af4819e4458e6cf177959674f04bc4b8
Kyle Ebbinga <26614720+TheGuitarleader@users.noreply.github.com>
2026-08-11 00:05:25 -0500

    Remove desktop app as its causing more issues currently

--- Parallel.Desktop/ViewModels/MainWindowViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright 2026 Entex Interactive
-
-namespace Parallel.Desktop.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- Parallel.Desktop/ViewModels/ViewModelBase.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright 2026 Entex Interactive
-
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Parallel.Desktop.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
